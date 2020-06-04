f4f04685c5a77f62ea49053398e13a9da41bda2a
asel <asel.kitulagoda@gmail.com>
2020-06-04 18:16:36 +0100

    added reroute

--- pages/profile.vue
+++ pages/profile.vue
@@ -129,7 +129,7 @@ export default {
     }
   },
     middleware: 'auth',
-    beforeMount(){this.reloadpage()},
+    mounted(){this.reloadpage()},
 
     computed: {
         ...mapGetters(['loggedInUser'])
